76a979a672c6d843b9e8e5c9bfdd7f1358c81e7c
suiminbudoku <zm19217@josai.ac.jp>
2021-10-12 02:07:52 +0000

    1012

---
+++ myjs.js
@@ -0,0 +1,11 @@
+alert(`hello,osamu!`)
+alert(`hello,sugimoto!`)
+alert(1+1);
+alert(Math.sqrt(2+3));
+alert(Math.sqrt(1/0));
+alert(Math.sqrt(0/0));
+alert(2+3);
+alert(`2`+`3`);
+alert(Number(`2`)+Number(`3`));
+alert(`2`+3);
+alert(Number(`2`)+3);
