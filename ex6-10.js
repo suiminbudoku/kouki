03783b6d5b0acf9c094a185e0d754e9fae1ba0dd
suiminbudoku <zm19217@josai.ac.jp>
2021-12-20 05:56:03 +0000

    1221

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,2 @@
-function imgchange1(){document.getElementById('logo').src = './image/Sakado_campus';}
+function imgchange1(){ document.getElementById('logo').src ='./image/josailogo.jpg';}
+function imgchange2(){ document.getElementById('logo').src ='./image/keiei_small.jpg';}
